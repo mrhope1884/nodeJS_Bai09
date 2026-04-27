4be653c732d977951e9b50f46b2fb87e00664935
mrhope1884 <vuvietquan1884@gmail.com>
2026-04-28 00:06:12 +0700

    update code

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 6969
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
-  const listUser = [
-    {
-      fullName: "Vũ Viết Quân",
-      email: "vuvietquan1884@gmail.com",
-      cccd: {
-        fullName: "Vũ Viết Quân",
-        number: "18/08/2004"
-      }
-    },
-    {
-      fullName: "Vũ Viết Quan",
-      email: "vietquan1884@gmail.com",
-    }
-  ];
-  console.log(listUser)
-  res.send('Hello World!')
+  res.render('index', { 
+    title: 'Trang chủ ',
+    message: 'Xin chào các bạn!' })
 })
 
+// app.get('/', (req, res) => {
+//   const listUser = [
+//     {
+//       fullName: "Vũ Viết Quân",
+//       email: "vuvietquan1884@gmail.com",
+//       cccd: {
+//         fullName: "Vũ Viết Quân",
+//         number: "18/08/2004"
+//       }
+//     },
+//     {
+//       fullName: "Vũ Viết Quan",
+//       email: "vietquan1884@gmail.com",
+//     }
+//   ];
+//   console.log(listUser)
+//   res.send('Hello World!')
+// })
+
+app.get('/Products', (req, res) => {
+  res.render('index', { 
+    title: 'Products ',
+    message: 'Trang danh sách sản phẩm ',
+    information: 'Thông tin '})
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
