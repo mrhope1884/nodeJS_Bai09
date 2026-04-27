25e3729824ddb249df997268a87da9406f1ac3a3
mrhope1884 <vuvietquan1884@gmail.com>
2026-04-27 22:36:46 +0700

    update code

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  const listUser = [
+    {
+      fullName: "Vũ Viết Quân",
+      email: "vuvietquan1884@gmail.com",
+      cccd: {
+        fullName: "Vũ Viết Quân",
+        number: "18/08/2004"
+      }
+    },
+    {
+      fullName: "Vũ Viết Quan",
+      email: "vietquan1884@gmail.com",
+    }
+  ];
+  console.log(listUser)
   res.send('Hello World!')
 })
 
